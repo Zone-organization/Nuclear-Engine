d32f6bc50c95c06c24de76044696ffd3df933f73
zlixine moro <zlixine@gmail.com>
2019-07-08 08:41:10 +0200

    Work on adding a checked configuration to the project.

--- Nuclear Engine/Source/Engine/PhysX/PhysXEngine.cpp
+++ Nuclear Engine/Source/Engine/PhysX/PhysXEngine.cpp
@@ -1,17 +1,17 @@
 #include "Engine\PhysX\PhysXEngine.h"
-#include "PhysX/include/PxPhysicsAPI.h"
-
-using namespace physx;
-PxDefaultAllocator		gAllocator;
-PxDefaultErrorCallback	gErrorCallback;
-
-PxFoundation* gFoundation = NULL;
-PxPhysics* gPhysics = NULL;
-
-PxDefaultCpuDispatcher* gDispatcher = NULL;
-#pragma comment(lib,"PhysX_64.lib")
-#pragma comment(lib,"PhysXFoundation_64.lib")
-#pragma comment(lib,"PhysXExtensions_static_64.lib")
+//#include "PhysX/include/PxPhysicsAPI.h"
+//
+//using namespace physx;
+//PxDefaultAllocator		gAllocator;
+//PxDefaultErrorCallback	gErrorCallback;
+//
+//PxFoundation* gFoundation = NULL;
+//PxPhysics* gPhysics = NULL;
+//
+//PxDefaultCpuDispatcher* gDispatcher = NULL;
+//#pragma comment(lib,"PhysX_64.lib")
+//#pragma comment(lib,"PhysXFoundation_64.lib")
+//#pragma comment(lib,"PhysXExtensions_static_64.lib")
 
 namespace NuclearEngine
 {
@@ -19,14 +19,14 @@ namespace NuclearEngine
 	{
 		bool PhysXEngine::Initialize(const PhysXEngineDesc& desc)
 		{
-			gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+	/*		gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
 			
 			gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true);
 			PxInitExtensions(*gPhysics, nullptr);
 
 			PxU32 numCores = 4;
 			gDispatcher = PxDefaultCpuDispatcherCreate(numCores == 0 ? 0 : numCores - 1);
-
+			*/
 			return true;
 		}
 	}
